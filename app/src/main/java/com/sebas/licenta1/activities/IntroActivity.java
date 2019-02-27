ca05605a32518e1bce91cb1c7f3d3e3b56b20308
Sebastian Coteanu <sebastian.coteanu@mindit.ro>
2019-02-27 02:09:07 +0200

    Add introActivity to display details about the app. Provide navigate options for signup/signin

--- app/src/main/java/com/sebas/licenta1/activities/IntroActivity.java
+++ app/src/main/java/com/sebas/licenta1/activities/IntroActivity.java
@@ -1,16 +1,15 @@
 package com.sebas.licenta1.activities;
 
-import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
-public class SplashActivity extends AppCompatActivity {
+import com.sebas.licenta1.R;
+
+public class IntroActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        Intent intent = new Intent(this, IntroActivity.class);
-        startActivity(intent);
-        finish();
+        setContentView(R.layout.activity_intro);
     }
 }
